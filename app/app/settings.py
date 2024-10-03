cc022d6c972daceb1affa6e7378245229f3f917a
ftmdhgh <dehghanfatemeh1400@gmail.com>
2024-10-04 00:42:51 +0330

    add django project

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reservationapi',
+        'USER': 'postgres',
+        'PASSWORD': 'f2031384',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
